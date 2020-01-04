a4ff000b592c6cd3d4ca09b1864cfa77a3424b41
ertugrulaydin <32276478+ertugrulaydin@users.noreply.github.com>
2020-01-05 00:58:31 +0300

    Add files via upload

---
+++ Snake Game/GameKontrol.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameKontrol : MonoBehaviour
+{
+    public GameObject yemPrefab;
+    Vector3 vec;
+    /*public Text ScoreText;
+    public Text GameOverText;
+    public Text GameOverScoreText;
+    bool scoregameover;*/
+    public YilanKontrol mainSnake;
+    /*float timeLeft;*/
+
+
+
+
+    void Start()
+    {
+        Vector3 vec = new Vector3(Random.Range(19.5f, -21.0f), 0.566f, Random.Range(14.0f, -15.0f));
+        Instantiate(yemPrefab,vec,Quaternion.identity);
+        mainSnake = GameObject.FindGameObjectWithTag("yilanBas").GetComponent<YilanKontrol>();
+    }
+    /*void FixedUpdate()
+    {
+        scorecontrol();
+
+        
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "yilanBas")
+        {
+            //mainSnake = GameObject.FindGameObjectWithTag("yilanBas").GetComponent<YilanKontrol>();
+            GameOverText.text = "GAME OVER";
+            GameOverScoreText.text = "Score: " + mainSnake.score;
+            scoregameover = true;
+            
+            mainSnake.hiz = 0;
+            timeLeft = 3.0f;
+
+        }
+        
+
+    }
+    void scorecontrol()
+    {
+        if (scoregameover)
+        {
+            ScoreText.text = " ";
+            timeLeft -= Time.deltaTime;
+            if (timeLeft < 0)
+            {
+                SceneManager.LoadScene("SceneOpen");
+            }
+        }
+        else
+        {
+            
+            ScoreText.text = "Score: " + mainSnake.score ;
+        }
+    }*/
+
+}
---
+++ Snake Game/Kuyruk.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Kuyruk : MonoBehaviour
+{
+    public float hiz;
+    public Vector3 hedefKuyruk;
+    public GameObject hedefKuyrukObj;
+    public YilanKontrol mainSnake;
+    public int index;
+    void Start()
+    {
+        mainSnake = GameObject.FindGameObjectWithTag("yilanBas").GetComponent<YilanKontrol>();
+        hiz = mainSnake.hiz + 2.5f;
+        hedefKuyrukObj = mainSnake.kuyrukObjects[mainSnake.kuyrukObjects.Count - 2];
+        index = mainSnake.kuyrukObjects.IndexOf(gameObject);
+
+    }
+
+    void Update()
+    {
+        hedefKuyruk = hedefKuyrukObj.transform.position;
+        transform.LookAt(hedefKuyruk);
+        transform.position = Vector3.Lerp(transform.position, hedefKuyruk, Time.deltaTime * hiz);
+    }
+}
